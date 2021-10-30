6b9c902a06c76c9dc68b82cfb7b9af5bf9adc93b
Jeremy Ong <jpongfam@gmail.com>
2021-10-30 16:01:32 +1100

    pwa using gatsby plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,28 @@
 module.exports = {
     siteMetadata: {
-        siteUrl: `https://www.yourdomain.tld`,
+        siteUrl: 'https://www.yourdomain.tld',
     },
-    plugins: [`gatsby-plugin-postcss`]
+    plugins: [{
+        resolve: 'gatsby-plugin-manifest',
+        options: {
+            name: 'GatsbyJS',
+            short_name: 'GatsbyJS',
+            start_url: '/',
+            background_color: '#f7f0eb',
+            theme_color: '#a2466c',
+            display: 'standalone',
+            icon: 'src/images/icon.png', // This path is relative to the root of the site.
+            cache_busting_mode: 'none',
+        },
+    },
+    {
+        resolve: 'gatsby-plugin-offline',
+        options: {
+            workboxConfig: {
+                globPatterns: ['**/icon-path*']
+            }
+        }
+    },
+        'gatsby-plugin-postcss',
+    ]
 }
