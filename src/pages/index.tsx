724702b69273a2f4e10e3c6a71bf87d91275da3a
jer <jpongfam@gmail.com>
2022-02-04 19:14:11 +1100

    styling home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,16 +11,24 @@ import { WeatherContext } from "../utilities/globalContext";
 
 export default function Index(props: any) {
   const { weather } = useContext(WeatherContext);
+  let date = new Date();
 
   return (
     <>
-      <header className="w-full py-5 shadow-md bg-gradient-to-b from-indigo-500 to-pink-500 text-white flex justify-evenly items-center">
-        <h1 className="font-bold text-3xl">
-          {`${weather.location.name}, ${weather.location.region}`}
-        </h1>
-        <Link to="/settings" className="">
+      <header className="content-full py-5 bg-gradient-to-b from-indigo-500 to-pink-500 flex justify-around items-center">
+        <div className="flex-col">
+          <h1 className="font-bold text-4xl">{weather.location.name}</h1>
+          <p className="font-bold italic text-xl">
+            {weather.location.region}
+          </p>
+        </div>
+        <div className="flex-col text-right">
+          <p className="font-bold italic text-2xl">{date.toLocaleTimeString()}</p>
+          <p className="font-bold italic text-md">{date.toLocaleDateString()}</p>
+        </div>
+        {/* <Link to="/settings" className="">
           <FontAwesomeIcon icon={faGear} className="scale-150" />
-        </Link>
+        </Link> */}
       </header>
       <pre className="container p-5">
         {JSON.stringify(weather.current, undefined, 2)}
