1341c7e977a81af06fd4a5994339c32190387f7b
Jeremy <jpongfam@gmail.com>
2022-02-20 14:52:38 +1100

    shows active page on footer

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -9,6 +9,9 @@ import { Link } from "gatsby";
 import React from "react";
 import ContextProvider from "../utilities/globalContext";
 
+const scale = "scale-150"
+const colour = "text-emerald-400"
+
 export default function Layout({ children }) {
   return (
     <ContextProvider>
@@ -16,17 +19,17 @@ export default function Layout({ children }) {
         {children}
 
         <footer className="bubble w-[calc(100%-2*theme(spacing.5))] m-5 py-5 bottom-0 fixed flex justify-evenly">
-          <Link to="/">
-            <FontAwesomeIcon icon={faTemperatureHalf} className="scale-150" />
+          <Link to="/" className={scale} activeClassName={colour}>
+            <FontAwesomeIcon icon={faTemperatureHalf} />
           </Link>
-          <Link to="/radar">
-            <FontAwesomeIcon icon={faSatelliteDish} className="scale-150" />
+          <Link to="/radar" className={scale} activeClassName={colour}>
+            <FontAwesomeIcon icon={faSatelliteDish} />
           </Link>
-          <Link to="/search">
-            <FontAwesomeIcon icon={faMagnifyingGlass} className="scale-150" />
+          <Link to="/search" className={scale} activeClassName={colour}>
+            <FontAwesomeIcon icon={faMagnifyingGlass} />
           </Link>
-          <Link to="/settings" className="">
-            <FontAwesomeIcon icon={faGear} className="scale-150" />
+          <Link to="/settings" className={scale} activeClassName={colour}>
+            <FontAwesomeIcon icon={faGear} />
           </Link>
         </footer>
       </div>
